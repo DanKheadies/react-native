4868710f75d2b5120d0cf735fe2c54f3e2c0ca0b
David Corso <davidcorso@Dankey-Kang.local>
2020-03-25 21:59:37 -0400

    RN015 Navigation app pre-favorite

---
+++ rn-navigation-app/components/MealItem.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native';
+
+const MealItem = props => {
+    return (
+        <View style={styles.mealItem}>
+            <TouchableOpacity onPress={props.onSelectMeal}>
+                <View>
+                    <View style={{...styles.mealRow, ...styles.mealHeader}}>
+                        <ImageBackground source={{uri: props.image}} style={styles.bgImage}>
+                            <View style={styles.titleContainer}>
+                                <Text style={styles.title} numberOfLines={1}>
+                                    {props.title}
+                                </Text>
+                            </View>
+                        </ImageBackground>
+                    </View>
+                    <View style={{...styles.mealRow, ...styles.mealDetails}}>
+                        <Text>{props.duration}m</Text>
+                        <Text>{props.complexity.toUpperCase()}</Text>
+                        <Text>{props.affordability.toUpperCase()}</Text>
+                    </View>
+                </View>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    bgImage: {
+        width: '100%',
+        height: '100%',
+        justifyContent: 'flex-end'
+    },
+    mealDetails: {
+        paddingHorizontal: 10,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        height: '15%'
+    },
+    mealHeader: {
+        height: '85%'
+    },
+    mealItem: {
+        height: 200,
+        width: '100%',
+        backgroundColor: '#fafafa',
+        // padding: 15,
+        marginBottom: 10,
+        borderRadius: 10,
+        overflow: 'hidden'
+    },
+    mealRow: {
+        flexDirection: 'row'
+    },
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 20,
+        color: 'white',
+        textAlign: 'center'
+    },
+    titleContainer: {
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        paddingVertical: 5,
+        paddingHorizontal: 12,
+    }
+});
+
+export default MealItem;
---
+++ rn-navigation-app/screens/CategoriesScreen.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { 
+    View, 
+    Text, 
+    Button, 
+    FlatList, 
+    StyleSheet,
+    TouchableOpacity
+} from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data';
+import CategoryGridTile from '../components/CategoryGridTile';
+
+const CategoriesScreen = props => {
+    const renderGridItem = (itemData) => {
+        return (
+            <CategoryGridTile 
+                title={itemData.item.title} 
+                color={itemData.item.color}
+                onSelect={() => {
+                    props.navigation.navigate({ 
+                        routeName: 'CategoryMeals', 
+                        params: {
+                            categoryId: itemData.item.id
+                        } 
+                    });
+                }} 
+            />
+        );
+    };
+
+    return (
+        // <View style={styles.screen}>
+        //     <Text>The Categories Screen!</Text>
+        //     <Button title="Go to Meals" onPress={() => {
+        //         // props.navigation.navigate({routeName: 'CategoryMeals'});
+        //         // props.navigation.push('CategoryMeals');
+        //         // props.navigation.replace('CategoryMeals');
+        //         props.navigation.navigate('CategoryMeals');
+        //     }} /> 
+        // </View>
+
+        <FlatList 
+            // keyExtractor={(item, index) => item.id}
+            data={CATEGORIES} 
+            renderItem={renderGridItem} 
+            numColumns={2} 
+        />
+    );
+};
+
+// CategoriesScreen.navigationOptions = {
+//     headerTitle: 'Meal Categories'
+// };
+
+const styles = StyleSheet.create({
+    screen: {
+        alignItems: 'center',
+        flex: 1,
+        justifyContent: 'center'
+    }
+});
+
+export default CategoriesScreen;
