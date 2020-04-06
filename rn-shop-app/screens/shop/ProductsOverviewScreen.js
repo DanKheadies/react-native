3b9851520e0ba93dcf38ca4e8bc6efd789d9267e
David Corso <davidcorso@Dankey-Kang.local>
2020-04-06 15:48:23 -0400

    RN017 Shop app initial project

---
+++ rn-shop-app/screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { 
+    FlatList, 
+    StyleSheet 
+} from 'react-native';
+
+const ProductsOverviewScreen = props => {
+    return (
+        <FlatList>
+            
+        </FlatList>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default ProductsOverviewScreen;
